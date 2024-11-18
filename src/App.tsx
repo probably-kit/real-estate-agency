e5b2f842ff42b14ca7b319add737bcc449f384f2
probably-kit <probably.kit@gmail.com>
2024-11-18 12:02:18 +0100

    add draft of nav-bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@ function App() {
 
   return (
     <>
-     <div>
-      <p>Oferty</p>
-      <p>Uslugi</p>
-      <p>O nas</p>
-     </div>
+      <nav className="nav-container">
+        <div className="nav-links">
+          <a>Oferty</a>
+          <a>Uslugi</a>
+          <a>O nas</a>
+        </div>
+        <img className="logo" src="/WERNER HOME Logo - Black with White Background - 5000x5000 (1).png" alt="logo"/> 
+        <div>Menu</div>
+
+      </nav>
+      <div>
+        <p></p>
+      </div>
     </>
   )
 }
