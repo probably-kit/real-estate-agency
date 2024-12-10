a50ac8a34b82c0a764caa1a9553246833a3ade6f
probably-kit <probably.kit@gmail.com>
2024-12-10 23:39:09 +0100

    fix svg's

--- src/components/StandardsGrid.tsx
+++ src/components/StandardsGrid.tsx
@@ -7,32 +7,32 @@ const StandardsGrid: React.FC = () => {
       <h1 className='standards-h1'>Highlights of Our Work Standards</h1>
       <section className="standards-grid">
         <div className="standards-grid-card">
-          <img src="src/assets/stats-chart-svgrepo-com.svg" alt="Market Analysis Icon" />
+          <img src="/stats-chart-svgrepo-com.svg" alt="Market Analysis Icon" />
           <h3>Market Analysis</h3>
           <p>In-depth understanding of market trends to guide pricing and strategy.</p>
         </div>
         <div className="standards-grid-card">
-          <img src="src/assets/house-lock-svgrepo-com.svg" alt="Property Valuation Icon" />
+          <img src="/house-lock-svgrepo-com.svg" alt="Property Valuation Icon" />
           <h3>Property Valuation</h3>
           <p>Accurate assessments to determine the true value of your favorite apartment, cottage, etc.</p>
         </div>
         <div className="standards-grid-card">
-          <img src="src/assets/file-check-svgrepo-com.svg" alt="Legal Assistance Icon" />
+          <img src="/file-check-svgrepo-com.svg" alt="Legal Assistance Icon" />
           <h3>Legal Assistance</h3>
           <p>Helping clients navigate the complexities of legal paperwork to ensure a smooth transaction.</p>
         </div>
         <div className="standards-grid-card">
-          <img src="src/assets/support-svgrepo-com.svg" alt="Post-Sale Support Icon" />
+          <img src="/support-svgrepo-com.svg" alt="Post-Sale Support Icon" />
           <h3>Post-Sale Support</h3>
           <p>Providing assistance even after the sale, ensuring clients feel supported throughout their journey.</p>
         </div>
         <div className="standards-grid-card">
-          <img src="src/assets/message-text-1-svgrepo-com.svg" alt="Negotiation Skills Icon" />
+          <img src="/message-text-1-svgrepo-com.svg" alt="Negotiation Skills Icon" />
           <h3>Negotiation Skills</h3>
           <p>In-depth understanding of market trends to guide pricing and strategy.</p>
         </div>
         <div className="standards-grid-card">
-          <img src="src/assets/target-bold-svgrepo-com.svg" alt="Tailored Marketing Plans Icon" />
+          <img src="/target-bold-svgrepo-com.svg" alt="Tailored Marketing Plans Icon" />
           <h3>Tailored Marketing Plans</h3>
           <p>Developing customized strategies to showcase properties and attract buyers.</p>
         </div>
