44db1dc727df7d38985df81e5e84a72e261bdd5a
probably-kit <probably.kit@gmail.com>
2025-01-07 22:50:00 +0100

    fixed deploy

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -2,34 +2,44 @@ import React from 'react';
 import './BlogCard.css';
 
 type BlogCardProps = {
-  title: string;
-  author: string;
-  date: string;
-  excerpt: string;
-  imageUrl: string;
-  onClick?: () => void;
-};
+    id: string; 
+    title: string;
+    author: string;
+    date: string;
+    excerpt: string;
+    imageUrl: string;
+    onClick?: () => void;
+  };
+  
 
 const BlogCard: React.FC<BlogCardProps> = ({
-  title,
-  author,
-  date,
-  excerpt,
-  imageUrl,
-  onClick
-}) => {
-  return (
-    <div className="blog-card" onClick={onClick} role="button">
-      <div className="blog-image">
-        <img src={imageUrl} alt={title} className="blog-thumbnail" />
+    id,
+    title,
+    author,
+    date,
+    excerpt,
+    imageUrl,
+    onClick
+  }) => {
+    return (
+      <div
+        className="blog-card"
+        onClick={onClick}
+        role="button"
+        data-blog-id={id} // Use it as a data attribute, for instance
+      >
+        <div className="blog-image">
+          <img src={imageUrl} alt={title} className="blog-thumbnail" />
+        </div>
+        <div className="blog-details">
+          <h3 className="blog-title">{title}</h3>
+          <p className="blog-author">
+            By {author} - {date}
+          </p>
+          <p className="blog-excerpt">{excerpt}</p>
+        </div>
       </div>
-      <div className="blog-details">
-        <h3 className="blog-title">{title}</h3>
-        <p className="blog-author">By {author} - {date}</p>
-        <p className="blog-excerpt">{excerpt}</p>
-      </div>
-    </div>
-  );
-};
-
+    );
+  };
+  
 export default BlogCard;
